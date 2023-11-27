749c953c8ae5e28eda281ff55dced9d17ff131ba
dryworts <omzg@mail.ru>
2023-11-27 22:49:46 +0600

    negative test added

--- src/test/java/ru/netology/pageobject/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/pageobject/test/MoneyTransferTest.java
@@ -43,5 +43,20 @@ public class MoneyTransferTest {
         assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
     }
 
+    @Test
+    void shouldGetErrorMessageIfAmountMoreBalance(){
+        var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+        var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+        var amount =  DataHelper.generateInvalidAmount(secondCardBalance);
+        var transferPage = dashboardPage.selectCardToTransfer(firstCardInfo);
+        transferPage.makeTransfer(String.valueOf(amount), secondCardInfo);
+        transferPage.findErrorMessage("Выполнена попытка перевода суммы, превышающей остаток на карте списания");
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(firstCardBalance, actualBalanceFirstCard);
+        assertEquals(secondCardBalance, actualBalanceSecondCard);
+
+    }
+
 
 }
